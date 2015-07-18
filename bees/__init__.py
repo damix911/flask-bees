b0c4b193ffa78837d79e43ac3ffe42be6a51cd74
Dario D'Amico <dario.damico@gmail.com>
2015-07-18 20:04:23 +0200

    Fixed setup.

--- bees/__init__.py
+++ bees/__init__.py
@@ -1,2 +1,1 @@
 from .bees import Bees
-from .info import version
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
-from bees import info
+
+version = "1.0.0"
 
 setup(
     name = "Flask-Bees",
-    version = info.version,
+    version = version,
     install_requires = [
         "Flask>=0.10.1",
         "Jinja2>=2.7.3",
